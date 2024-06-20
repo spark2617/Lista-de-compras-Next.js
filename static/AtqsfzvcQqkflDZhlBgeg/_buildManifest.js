a2cbf337afc298658d83f2c453bd207662b595fe
jhonatan <jhonatandesouzade13@gmail.com>
2024-06-19 20:51:19 -0400

    Updates

---
+++ static/AtqsfzvcQqkflDZhlBgeg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/803a7aecc56a79ca.css","static/chunks/pages/index-d32a2d8e6d37c4e3.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
